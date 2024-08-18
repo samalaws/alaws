a65c22dca55e6d5d03f9f7afcdd77d2e8290e50f
Samer <samalaws@gmail.com>
2024-08-18 13:06:47 +0200

    change redirect url

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,32 +1,45 @@
 import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
-import { unstable_noStore as noStore}  from "next/cache";  
+import { unstable_noStore as noStore } from "next/cache";
 
-export async function GET(){
-    noStore();
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+export async function GET() {
+  noStore();
+  const { getUser } =
+    getKindeServerSession();
+  const user = await getUser();
 
-    if (!user || user.email !== process.env.ADMIN_EMAIL) {
-        throw new Error("Unauthorized");
-    }
-    let dbUser = await prisma.user.findUnique({
-        where:{
-            id: user.id
-        },
+  if (
+    !user ||
+    user.email !==
+      process.env.ADMIN_EMAIL
+  ) {
+    throw new Error("Unauthorized");
+  }
+  let dbUser =
+    await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
     });
 
-    if (!dbUser) {
-        dbUser = await prisma.user.create({
-            data:{
-                id: user.id,
-                firstName: user.given_name ?? "",
-                lastName: user.family_name ?? "",
-                email: user.email ?? "",
-            },
-        });
-    }
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName:
+          user.given_name ?? "",
+        lastName:
+          user.family_name ?? "",
+        email: user.email ?? "",
+      },
+    });
+  }
 
-    return NextResponse.redirect('http://localhost:3000/')
+  return NextResponse.redirect(
+    process.env.NODE_ENV ===
+      "production"
+      ? "https://alaws.vercel.app/"
+      : "http://localhost:3000"
+  );
 }
