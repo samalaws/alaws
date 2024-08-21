1c00a30bb63eeaff8aef58fdc7b7f8cdf134b05e
Samer <samalaws@gmail.com>
2024-08-21 10:36:12 +0200

    edit css layout page

--- app/[lang]/(landpage)/layout.tsx
+++ app/[lang]/(landpage)/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider 
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
--- app/components/landpage/Navbar.tsx
+++ app/components/landpage/Navbar.tsx
@@ -12,26 +12,28 @@ export default function Navbar({
   return (
     <>
       <nav className="hidden md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        <div className="max-w-7xl mx-auto py-10 flex flex-col md:flex-row justify-between items-center px-4">
-          <div className="flex flex-shrink-0 text-center md:text-left">
-            <Link
-              href="/"
-              >
-              <h1 className="text-3xl font-thin">
-                Samer
-                <span className="text-3xl font-bold">
-                  Alaws
-                </span>
-              </h1>
-              </Link>
+        <div className="max-w-7xl mx-auto">
+          <div className="py-10 flex flex-col md:flex-row justify-between items-center px-4">
+            <div className="flex flex-shrink-0 text-center md:text-left">
+              <Link
+                href="/"
+                >
+                <h1 className="text-3xl font-thin">
+                  Samer
+                  <span className="text-3xl font-bold">
+                    Alaws
+                  </span>
+                </h1>
+                </Link>
             </div>
-            <div className="flex flex-grow justify-center ">
+            <div className="flex flex-grow items-center justify-center ">
               <Header lang={params.lang}/>
             </div>
             <div className="flex flex-shrink-0 items-center">
               <LocaleSelecter/>
               <ThemeSwitcher/>
             </div>
+          </div>
         </div>
       </nav>  
     </>
