ea2525f39db487f938ce3550b5b71868868da071
Samer <samalaws@gmail.com>
2024-08-23 07:50:43 +0200

    edit metadata

--- app/[lang]/(landpage)/page.tsx
+++ app/[lang]/(landpage)/page.tsx
@@ -1,9 +1,20 @@
 import Hero from "@/app/components/landpage/Hero";
 
 export const metadata = {
-    title: "Samer Alaws",
-    description: "Full Stack Web Developer, 52511 Geilenkirchen, Germany",
-}
+    title: "Samer Alaws | Full Stack Web Developer (Next.js, React)",
+    description: "Full Stack Web Developer specializing in modern technologies like Next.js and React.js. Build high-performance web applications.",
+    keywords: "Full Stack Web Developer, Next.js, React.js, TypeScript, MongoDB, JavaScript",
+    themeColor: "black",
+    colorScheme: "dark",
+    openGraph: {
+      title: "Samer Alaws | Full Stack Web Developer",
+      description: "Full Stack Web Developer specializing in modern technologies like Next.js and React.js. Build high-performance web applications.",
+      url: "https://alaws.de",
+      siteName: "Samer Alaws",
+      locale: "en-US, de-DE, ar-AR",
+      type: "website",
+    },
+  };
 
 export default function Home() {
     return (
